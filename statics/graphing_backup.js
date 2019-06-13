e309a8d1861be71456be8875a8b8a786c1a77b09
Phelps <clytze20@gmail.com>
2019-06-13 00:27:23 -0700

    debug

--- statics/graphing_backup.js
+++ statics/graphing_backup.js
@@ -81,7 +81,7 @@ function update(coords, routeName, clickedIndex) {
     timeout = null;
     p(routeInfo[routeName]['fullname'].replace('|','_'))
     frames_url = "statics/img/"+routeInfo[routeName]['fullname'].replace('|','_');
-    video_URL = frames_url+'-'+marker['lat'].toFixed(4)+'-'+marker['lng'].toFixed(4)+".jpg";
+    video_URL = frames_url+'-'+marker['index']/100+".jpg";
 
     video_pic.src = video_URL
     video_pic.onerror = function() {
